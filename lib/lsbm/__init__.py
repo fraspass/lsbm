43dbf5110ead585d8fffcee003204dc8b04acdb0
fraspass <fraspass@outlook.com>
2021-12-15 23:16:27 +0000

    transformed repo into package

---
+++ lib/lsbm/__init__.py
@@ -0,0 +1,13 @@
+from .lsbm_gp import (
+	delete_inverse,
+	add_inverse,
+	lsbm_gp_gibbs
+)
+
+from .lsbm import (
+	lsbm_gibbs
+)
+
+from .utilities import *
+
+from .zhu import *
---
+++ lib/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='lsbm',
+	version='1.0',
+	packages=[
+		'lsbm',
+	],
+	install_requires=[
+		'numpy',
+		'scipy',
+		'sklearn',
+	],
+)
